3db727e66b49a4d63fd746d15d529c3e5f118932
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-12-23 16:36:39 +0700

    center guessed month text to its parent

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -23,9 +23,13 @@ export default function Play() {
             <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
             <div className="ml-10 mr-10 animate-fade-in grid justify-center">
                 <iframe className="w-[40vw] h-[23vw] my-8" src="https://www.youtube.com/embed/kHh7LMzlKRI" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-                <h2 className="my-8 text-xl text-white text-center cursor-default text-edge-outline animate-title font-display">Youtube title videos here</h2>
-                <center className="text-white text-lg font-display relative inline-block w-[12vw] h-[4vw] bg-red-500 mb-5 rounded-lg place-self-center">
-                    <div className="text-center h-full">{getGuessedDate(sliderVal)}</div>
+                <h2 className="my-8 text-xl text-white text-center cursor-default text-edge-outline animate-title font-display">
+                    Youtube title videos here
+                </h2>
+                <center className="flex text-white text-lg font-display w-[12vw] h-[4vw] bg-red-500 mb-5 rounded-lg place-self-center">
+                    <div className="text-center m-auto">
+                        {getGuessedDate(sliderVal)}
+                    </div>
                 </center>
                 <DateSlider className="mb-10" value={[sliderVal]} onValueChange={(val) => setSliderVal(val[0])} />
                 <Button
