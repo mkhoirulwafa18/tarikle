5efa3ff628ec691c1651e4da0aac3a37718323d8
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-12-27 16:06:28 +0700

    update ui home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 
-import { InstructionCard } from "@/components/instruction-card";
+import { Button } from "@/components/ui/button";
 import Particles from "@/components/ui/particles";
+import { Separator } from "@/components/ui/separator";
+import { useRouter } from "next/navigation"
+import Image from "next/image";
 import React from "react";
+import { Switch } from "@/components/ui/switch";
+import { addStorage, getStorage } from "@/lib/storage-service";
 
 export default function Home() {
+
+  const [hardmode, setHardmode] = React.useState(false);
+  const router = useRouter();
+
+  React.useEffect(() => {
+    const isHard = getStorage('hard-mode');
+    setHardmode(isHard == '1' ? true : false)
+  }, [])
+
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
@@ -12,13 +26,58 @@ export default function Home() {
         className="absolute inset-0 -z-10 animate-fade-in"
         quantity={300}
       />
-      <h1 className="z-10 text-4xl text-transparent bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        tarikle
-      </h1>
+      <div className="z-10 text-4xl text-transparent bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text">
+        {/* tarikle */}
+        <Image
+          src='/tarikguessr2.png'
+          alt=''
+          width={600}
+          height={170}
+          className="rounded-md object-cover mx-auto"
+        />
+      </div>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
-      <div className="animate-fade-in">
-        <InstructionCard />
+      <div className="animate-fade-in flex">
+        {/* <InstructionCard /> */}
+        <Button
+          size="lg"
+          variant="link"
+          className="text-white hover:text-red-500 font-sans text-2xl"
+          onClick={() => {
+            addStorage('isDaily', '0')
+            router.push('/play')
+          }}
+        >
+          PLAY
+        </Button>
+        <Separator orientation="vertical" />
+        <div className="flex flex-col text-center animate-fade-in items-center mx-8">
+          <h2 className="text-sm text-white ">
+            Hard Mode
+          </h2>
+          <Switch
+            id="hard-mode"
+            checked={hardmode}
+            onCheckedChange={() => {
+              addStorage('hard-mode', !hardmode ? '1' : '0');
+              setHardmode(!hardmode);
+            }}
+
+          />
+        </div>
+        <Separator orientation="vertical" />
+        <Button
+          size="lg"
+          variant="link"
+          className="text-red-500 hover:text-white font-sans text-2xl"
+          onClick={() => {
+            addStorage('isDaily', '1')
+            router.push('/play')
+          }}
+        >
+          DAILY
+        </Button>
       </div>
     </div>
   );
