9278dc50c2576da464be128da24e38b179c5eecc
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-12-28 16:01:33 +0700

    remove description from Video interface and added LeaderboardItem interface

--- types.ts
+++ types.ts
@@ -2,5 +2,11 @@ export interface Video {
     videoId: string,
     publishedAt: string,
     title: string,
-    description: string
+}
+
+export interface LeaderBoardItem {
+    name: string,
+    socialMediaLink: string,
+    labelSocialMedia: string,
+    score: string
 }
