1efb5d1483b1759048c4e2e6428b393cacb978a4
mkhoirulwafa18 <muhkhwafa@gmail.com>
2023-12-28 16:05:47 +0700

    avoiding duplicate videos in a game

--- app/api/play/route.ts
+++ app/api/play/route.ts
@@ -4,8 +4,15 @@ import { promises as fs } from 'fs';
 export async function GET(
 ) {
     try {
+        let result = [];
         const file = await fs.readFile(process.cwd() + '/app/data/vids.json', 'utf8');
-        const result = JSON.parse(file)
+        const fileData = JSON.parse(file)
+        do {
+            const index = Math.floor(Math.random() * fileData.length)
+            const video = fileData[index];
+            result.push(video)
+        } while (result.filter((val, id, array) => array.indexOf(val) == id).length < 5);
+        result = result.filter((val, id, array) => array.indexOf(val) == id);
         return NextResponse.json(result);
     } catch (error) {
         console.log('[PLAY_GET]', error)
