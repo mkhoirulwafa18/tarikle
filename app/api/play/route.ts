da3c729802456527a13c5478a96efd6f5ed8f762
Muhammad Khoirul Wafa <muhkhwafa@gmail.com>
2024-01-10 02:42:15 +0700

    Update route.ts
    remove dynamic segment and replaced with revalidate with value 2(seconds)

--- app/api/play/route.ts
+++ app/api/play/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { promises as fs } from 'fs';
 
-export const dynamic = 'force-dynamic'
+export const revalidate = 2
 export async function GET(
 ) {
     try {
